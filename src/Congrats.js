b8398da8fcf3664985f07496467ee29d21086d1a
ANS <36783010+answebdev@users.noreply.github.com>
2021-01-22 18:57:00 -0800

    initial commit, add Enzyme

---
+++ src/Congrats.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// JS Doc:
+/**
+ * Functional React component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'sucess' prop is false)
+ */
+
+const Congrats = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Congrats
