6b834f97c904eb0faf9df2c0a21cebea69b785f7
ANS <36783010+answebdev@users.noreply.github.com>
2021-02-04 20:17:07 -0800

    add Bootstrap styling

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -10,15 +10,15 @@ const GuessedWords = (props) => {
   } else {
     const guessedWordsRows = props.guessedWords.map((word, index) => (
       <tr data-test='guessed-word' key={index}>
-        <td>word.guessedWord</td>
-        <td>word.letterMatchCount</td>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
       </tr>
     ));
     contents = (
       <div data-test='guessed-words'>
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className='table table-sm'>
+          <thead className='thead-light'>
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
